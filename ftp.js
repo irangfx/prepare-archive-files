8a13c4ec4e34d24d68984b8a45d9d9c4375699b7
HamidNE <hamidne@mail.ru>
2019-05-29 11:56:52 +0430

    edit for ftp file config

--- ftp.js
+++ ftp.js
@@ -1,32 +1,42 @@
 require('dotenv').config()
 
-var fs = require('fs');
-var Client = require('ftp');
+const fs = require('fs');
+const Client = require('ftp');
 
 // SOURCE FTP CONNECTION SETTINGS
-var srcFTP = {
+const srcFTP = {
     host: process.env.FTP_HOST,
     port: process.env.FTP_PORT,
     user: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD
 }
 
-var c = new Client();
+const c = new Client();
+const downloadList = [];
 
 c.on('ready', function () {
-    const file = 'index.html';
-
-    // c.get('public_html/' + file, function (err, stream) {
-    //     if (err) throw err;
-    //     stream.once('close', function () { c.end(); });
-    //     stream.pipe(fs.createWriteStream(file));
-    // });
-
-    c.put('foo.txt', 'public_html/foo.txt', function (err) {
+    c.list('public_html/premium/wall', function (err, list) {
         if (err) throw err;
-        console.log('asdasd');
+
+        list.map(function (entry) {
+            if (entry.name.match(/\.rar$/))
+                downloadList.push(entry.name);
+        });
+
+        downloadList.map(function (file) {
+            c.get('public_html/premium/wall' + file, function (err, stream) {
+                if (err) throw err;
+                stream.once('close', function () { c.end(); });
+                stream.pipe(fs.createWriteStream(file));
+            });
+        });
+
         c.end();
     });
+});
+
+c.on('end', function () {
+    console.log(downloadList);
 });
 
 c.connect(srcFTP);
