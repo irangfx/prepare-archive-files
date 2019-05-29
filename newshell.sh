8349c548694b79366cc8eaf66698bf4851cb7383
HamidNE <hamidne@mail.ru>
2019-05-29 09:42:36 +0430

    fixed extract archive file

--- newshell.sh
+++ newshell.sh
@@ -2,7 +2,7 @@ oldPassword='tarhan.ir';
 newPassword='irangfx.com';
 
 mkdir "$2"
-unrar "e -p$oldPassword $1 ./$2"
-cd "$2"
-mv Tarhan.ir.url IranGFX.com.url
-sed "'s+www.tarhan.ir/academy+irangfx.com+g' IranGFX.com.url"
+unrar e -p$oldPassword $1 ./$2
+cd $2
+sed 's+www.tarhan.ir/academy+irangfx.com+g' Tarhan.ir.url > IranGFX.com.url
+rm Tarhan.ir.url
