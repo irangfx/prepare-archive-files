a7a3a570c42fd3e0db0e92fc9cb34717664b064f
HamidNE <hamidne@mail.ru>
2019-05-29 10:37:00 +0430

    edit for rename archive files

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var fs = require('fs');
 var path = require('path');
 const { exec } = require('child_process');
 
 const oldPassword = 'tarhan.ir';
 const newPassword = 'irangfx.com';
 
-const file = 'Creative-Designers-Icons.tarhan.ir.rar';
 
-const extension = path.extname(file);
-const newName = file.replace('tarhan.ir', 'irangfx.com').replace(extension, '');
+const files = []
+files.push('Creative-Designers-Icons.tarhan.ir.rar');
+files.push('Anniversary-Invitation.tarhan.ir.rar');
+files.push('Snowdrops-Photo-Overlays.tarhan.ir.rar');
+files.push('Soccer-Flyer-5.tarhan.ir.rar');
 
-if (extension === '.rar') {
-    exec(`./newshell.sh '${file}' '${newName}'`);
-} else if (extension === '.zip') {
-
-}
-
-async function extractArchive() {
-    await spawn(`unrar e -p${oldPassword} ${file} ./${newName}`);
-    await spawn(`mv ./${newName}/Tarhan.ir.url ./${newName}/IranGFX.com.url`);
-    await spawn(`sed 's+www.tarhan.ir/academy+irangfx.com+g' ./${newName}/IranGFX.com.url`);
-}
+files.forEach(file => {
+    const extension = path.extname(file);
+    const newName = file.replace('tarhan.ir', 'irangfx.com').replace(extension, '');
+    if (extension === '.rar') {
+        exec(`./rar-extractor.sh '${file}' '${newName}'`);
+    } else if (extension === '.zip') {}
+});
--- rar-extractor.sh
+++ rar-extractor.sh
@@ -1,8 +1,11 @@
 oldPassword='tarhan.ir';
 newPassword='irangfx.com';
 
-mkdir "$2"
+mkdir $2
 unrar e -p$oldPassword $1 ./$2
 cd $2
 sed 's+www.tarhan.ir/academy+irangfx.com+g' Tarhan.ir.url > IranGFX.com.url
 rm Tarhan.ir.url
+cd ..
+rar -p$newPassword -r a $2.rar $2
+rm -rf $2
